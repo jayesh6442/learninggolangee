f5acf3864fab3c2d95f07c82b2d32e0e97dd215f
jayesh <jayeshsarvaiya9913@gmail.com>
2026-08-14 20:22:02 +0530

    changed code

--- main.go
+++ main.go
@@ -1,18 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"time"
-
-	"github.com/google/uuid"
-	"github.com/jayesh6442/onedarkage/storage/one"
-)
+import "fmt"
 
 func main() {
-	fmt.Println("hi there we are in the go code base and we are here for end to end master go and make some more monkey")
-	one.One()
-	val := uuid.New()
-	fmt.Println(val)
-	go fmt.Println("hi there we are in gorotien")
-	time.Sleep(1 * time.Second)
+	fmt.Println("hi there we are in go lang")
 }
