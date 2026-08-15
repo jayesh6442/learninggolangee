9a01d5534b1f5894abb8f7677bc5d35adee79247
jayesh <jayeshsarvaiya9913@gmail.com>
2026-08-15 11:06:36 +0530

    data type added

--- main.go
+++ main.go
@@ -1,22 +1,22 @@
 package main
 
-import (
-	"fmt"
-	"os"
-)
+import "fmt"
 
 func main() {
-	err := readFromFile()
-	if err != nil {
-		os.Exit(1)
-	}
-}
+	// data type in go and how to use it
+	total := 23 // direct infer as int data type
+	fmt.Println(total)
+	var x int8 = 124
+	var y int16 = 32000
+	var z int32 = 342342344
+	var a int64 = 234324234
 
-func readFromFile() error {
-	file, err := os.Create("one.txt")
-	if err != nil {
-		return fmt.Errorf("error reading file", err)
-	}
-	defer file.Close()
-	return nil
+	var abc uint = 34234234 // no nagative for unsigned data type
+
+	var onte uint8 = 124    // no nagative for unsigned data type
+	var zy uint16 = 3423    // no nagative for unsigned data type
+	onerauint32 := 34234234 // no nagative for unsigned data type
+
+	var teston uint64 = 34234234 // no nagative for unsigned data type
+	fmt.Println(x, y, z, a, abc, onte, zy, onerauint32, teston)
 }
