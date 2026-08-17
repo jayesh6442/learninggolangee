5b567dc529e3087449adbadba6103e3a3e3990fd
jayesh <jayeshsarvaiya9913@gmail.com>
2026-08-17 19:19:26 +0530

    ibe

--- main.go
+++ main.go
@@ -1,17 +1,4 @@
 package main
 
-import "fmt"
-
-var test int = 23
-
-// var onetest := 243;
 func main() {
-	fmt.Println(onetest)
-	fmt.Println(test)
-	test := 23
-	test = 34
-	fmt.Println(test)
-	var one int16 = 234
-	one = 222
-	fmt.Println(one)
 }
